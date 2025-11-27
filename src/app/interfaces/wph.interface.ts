0e976e71574c866eee5014df2eea44193a36c790
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-11-27 15:02:25 -0600

    interfaces to connect api

--- src/app/interfaces/wph.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface WPH {
-    id: number;
-    title: string;
-}
--- src/app/services/wp.service.ts
+++ src/app/services/wp.service.ts
@@ -1,31 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { WPApiResponse } from '../interfaces/wpapi.interface';
-import { WPH } from '../interfaces/wph.interface';
-import { WPHMapper } from '../mapper/wph.mapper';
+// import { HttpClient } from '@angular/common/http';
+// import { inject, Injectable, signal } from '@angular/core';
+// import { environment } from '../../environments/environment';
+// import { WPH } from '../interfaces/wph.interface';
 
 
-@Injectable({ providedIn: 'root' })
-// Wordpress Headless API Service Class
-export class WPService {
+// @Injectable({ providedIn: 'root' })
+// // Wordpress Headless API Service Class
+// export class WPService {
 
-  private http = inject(HttpClient)
-  private domain = environment.WP_DOMAIN
-  private apiUrl = `${this.domain}/wp-json/wp/v2`
+//   private http = inject(HttpClient)
+//   private domain = environment.WP_DOMAIN
+//   private apiUrl = `${this.domain}/wp-json/wp/v2`
 
-  wPHeadless = signal<WPH[]>([])
-  wPHeadlessLoading = signal(true)
+//   wPHeadless = signal<WPH[]>([])
+//   wPHeadlessLoading = signal(true)
 
-  constructor() {
-  }
+//   constructor() {
+//   }
 
-  loadWPHeadless(slug: string) {
+//   loadWPHeadless(slug: string) {
 
-    this.http.get<WPApiResponse>(`${this.apiUrl}/pages?slug=${slug}`)
-      .subscribe((resp) => {
-        console.log(resp)
-        this.wPHeadlessLoading.set(false)
-      })
-  }
-}
+//     this.http.get<WPApiResponse>(`${this.apiUrl}/pages?slug=${slug}`)
+//       .subscribe((resp) => {
+//         console.log(resp)
+//         this.wPHeadlessLoading.set(false)
+//       })
+//   }
+// }
